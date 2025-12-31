3f3bcb160ce9704dc1bc2635b593cc1ef2bfd85b
Anushka Negi <anushka27.negi@gmail.com>
2025-12-31 20:22:38 +0530

    Add volunteer dashboard components and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import AdminDashboard from "./pages/AdminDashboard";
+import VolunteerDashboard from "./pages/VolunteerDashboard";
+
 function App() {
 
-  return <AdminDashboard/>
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard/>}/>
+        <Route path="/volunteer" element={<VolunteerDashboard/>}/>
+        
+      </Routes>
+    </BrowserRouter>
+     
+  
+  );
 }
 
-export default App
+export default App;
